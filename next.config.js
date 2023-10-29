3352fa5e44cfc088ed4bd3ad68d6aac9c818acb3
antonio.natilla <antonio.natilla30@gmail.com>
2023-10-29 17:56:23 +0100

    Setup for static deployment, Remark dependency
    Added the necessary configuration for static deployment, and notes about
    its process.
    Removed package-lock.json that was incorrectly added to versioning.
    Added a dependency on Remark for converting markdown text to HTML pages.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    // NOTE: these configuration entries allow for static exports; see this page 
+    // for details:
+    // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+    // `distDir` allows to specify the output directory of the static deployment.
+    output: "export",
+    distDir: "out"
+}
 
 module.exports = nextConfig
