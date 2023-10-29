5d7eb0e0db60b1baade2f5c4bdb1498f3d33759a
antonio.natilla <antonio.natilla30@gmail.com>
2023-10-29 19:19:54 +0100

    Proof-of-concept: file parsing and listing
    Created a proof-of-concept for the main page.
    The idea is iterating on the _posts directory (and possible
    sub-directories) in the server, parsing each file and visualizing a
    list of available blog-posts.
    This is done by processing the old format that the blog supported when
    using Jekyll and the "standard" configuration for Github Pages.
    
    Note that, in the future, we might need to break this list down in more
    pages. That might be challenging if there is no way to produce multiple
    pages (or sub-pages) from this initial one.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,85 @@
-import Image from 'next/image'
+import { readdir, readFile} from 'fs/promises';
+import { join } from 'path';
 
-export default function Home() {
+export default async function Home() {
+
+  const posts_directory : string = "_posts";
+  const files = (await readdir(posts_directory)).map(filename => join(posts_directory, filename));
+  console.log(files);
+
+  const processed_files = files.map(
+    async (filename : string) : Promise<[string, string]> => {
+      const file_content : string = await readFile(filename, {encoding: "utf8"});
+
+      const find_title = () => {
+        const title_marker = "title:";
+        const title_entry_start : number = file_content.indexOf(title_marker);
+        const title_entry_end : number = file_content.indexOf("\n", title_entry_start);
+
+        return file_content.substring(title_entry_start + title_marker.length, title_entry_end).trim();
+      }
+
+      const find_excerpt = () => {
+        const excerpt_marker_entry_marker : string = "excerpt_separator:";
+        const excerpt_marker_start : number = file_content.indexOf(excerpt_marker_entry_marker);
+        const excerpt_marker_end : number = file_content.indexOf("\n", excerpt_marker_start);
+        const content_start_marker : string = "---\n\n";
+        const content_start : number = file_content.indexOf(content_start_marker)
+
+        if(content_start == -1) {
+          return "Fake excerpt because origin file was malformed."
+        }
+
+        const excerpt_not_found = (excerpt_marker_end == -1 || excerpt_marker_start == -1);
+        if(excerpt_not_found) {
+          const custom_excerpt_end : number = file_content.indexOf("\n\n", content_start);
+          return file_content.substring(content_start, custom_excerpt_end);
+        } else {
+          const excerpt_marker : string = file_content.substring(excerpt_marker_start + excerpt_marker_entry_marker.length, excerpt_marker_end).trim();
+          const excerpt_end : number = file_content.indexOf(excerpt_marker, excerpt_marker_end);
+          return file_content.substring(content_start + content_start_marker.length, excerpt_end);
+        }
+      }
+
+      return [find_title(), find_excerpt()]
+    }
+  );
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-3xl lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-400 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        # Latest posts
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-rows-4 lg:text-left">
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {
+        processed_files.map( async (promise) => {
+          const [title, content] = await promise;
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
+          return <a
+          href="."
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            {title}{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              {"->"}
+            </span>
+          </h2>
+          <p className={`m-0 text-sm opacity-50`}>
+            {content}
+          </p>
+        </a>
+        })
+        
+        }
+
+
+        {/* <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -106,7 +145,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
-        </a>
+        </a> */}
       </div>
     </main>
   )
