19b7624c8c435511a5b4f3e1eb161f2633c08190
antonio.natilla <antonio.natilla30@gmail.com>
2023-12-23 19:43:19 +0100

    Folder cleanup, adding new icons, refining layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,17 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
-import { Suspense } from "react";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "A clueless programmer's blog",
+	title: "A Sly Blog",
 	description:
-		"The blog where Antonio stores his learning journey and thoughts about programming",
+		"A blog where a clueless programmer shares his learning journey and thoughts about programming",
+	icons: {
+		icon: "/public/favicon-32x32.png",
+	},
 };
 
 export default function RootLayout({
@@ -19,8 +21,40 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
+		<>
+			<Head>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="public/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="public/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="public/favicon-16x16.png"
+				/>
+
+				<link rel="manifest" href="public/site.webmanifest" />
+				<link
+					rel="mask-icon"
+					href="public/safari-pinned-tab.svg"
+					color="#5bbad5"
+				/>
+				<link rel="shortcut icon" href="public/favicon.ico" />
+				<meta name="msapplication-TileColor" content="#da532c" />
+				<meta name="msapplication-config" content="public/browserconfig.xml" />
+				<meta name="theme-color" content="#ffffff" />
+			</Head>
+			<html lang="en">
+				<body className={inter.className}>{children}</body>
+			</html>
+		</>
 	);
 }
