dbe8e2e8e1d46905d4bd093281ef69bcb6a64f94
antonio.natilla <antonio.natilla30@gmail.com>
2023-12-23 18:32:55 +0100

    TextParagraph component, styling landing page
    - Created a TextParagraph component whose styling can be overridden from
      its parent.
    - Animated the hovering on the previews of a blog post.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,26 @@
-"use client"
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+"use client";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
 import { Suspense } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "A clueless programmer's blog",
-  description: "The blog where Antonio stores his learning journey and thoughts about programming",
-}
+	title: "A clueless programmer's blog",
+	description:
+		"The blog where Antonio stores his learning journey and thoughts about programming",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
