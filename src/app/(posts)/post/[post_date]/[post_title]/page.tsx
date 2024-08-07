6d88cdf8c5718cb09a9352e79be95ff925485366
antonio.natilla <antonio.natilla30@gmail.com>
2024-08-07 18:06:13 +0200

    Showing post content, applying custom styling
    The post page now shows the styled title and the post content, with
    some basic styling applied.
    
    NOTE: The styling is temporarily copied from mdx-components.tsx; it
    should easily become a configuration file that can be imported
    wherever it is needed.

--- src/app/(posts)/post/[post_date]/[post_title]/page.tsx
+++ src/app/(posts)/post/[post_date]/[post_title]/page.tsx
@@ -11,6 +11,9 @@ import {
 
 import { join } from "path";
 
+// TEMPORARILY
+import TextParagraph from "@/app/components/text_paragraph";
+
 export const dynamic = "force-static";
 export const dynamicParams = false;
 
@@ -56,7 +59,62 @@ export default async function Page({
 	const { content, frontmatter } = await compileMDX<PostFrontmatter>({
 		source: file,
 		options: { parseFrontmatter: true },
+		components: {
+			p: ({ children }) => {
+				return (
+					<TextParagraph
+						text={children as string}
+						classes="text-base text-gray-300 text-justify"
+					/>
+				);
+			},
+
+			ul: ({ children }) => {
+				const ul_class = "text-base py-2 mx-2";
+				if (Array.isArray(children)) {
+					return <ul className={ul_class}>{...children}</ul>;
+				}
+				return <ul className={ul_class}>{children}</ul>;
+			},
+
+			li: ({ children }) => {
+				const li_classes =
+					"text-base list-disc list-inside m-0 mr-2 text-sm text-gray-300";
+				if (Array.isArray(children)) {
+					return <li className={li_classes}>{...children}</li>;
+				}
+				return <li className={li_classes}>{children}</li>;
+			},
+
+			a: ({ href, children: child }) => {
+				return (
+					<a
+						href={href}
+						className="text-[#ffcc00] hover:bg-gradient-to-r 
+						hover:from-[#ffcc00] hover:via-yellow-200 hover:to-[#ffcc00]
+						hover:text-transparent hover:bg-clip-text hover:via-10%"
+					>
+						{child}
+					</a>
+				);
+			},
+
+			h2: ({ children }) => {
+				const h2_classes = "text-xl ms-2 py-1 text-[#eebb00]";
+				if (Array.isArray(children)) {
+					return <h2 className={h2_classes}>({...children})</h2>;
+				}
+				return <h2 className={h2_classes}>{children}</h2>;
+			},
+		},
 	});
 
-	return <>{frontmatter.title}</>;
+	return (
+		<>
+			<div className="text-3xl font-semibold text-amber-500">
+				{frontmatter.title}
+			</div>
+			{content}
+		</>
+	);
 }
