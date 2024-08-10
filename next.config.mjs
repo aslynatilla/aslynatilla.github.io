aa231f773508fdf29037a1156f31eccca80611f1
antonio.natilla <antonio.natilla30@gmail.com>
2024-08-10 16:41:00 +0200

    Changes to generate the out directory properly

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 	// NOTE: these configuration entries allow for static exports; see this page
@@ -6,8 +6,14 @@ const nextConfig = {
 	// https://nextjs.org/docs/app/building-your-application/deploying/static-exports
 	// `distDir` allows to specify the output directory of the static deployment.
 	output: "export",
-	basePath: "/aslynatilla.github.io",
+	// basePath: "/aslynatilla.github.io",
+	distDir: "out",
 	images: {
 		unoptimized: true,
 	},
 };
+
+export default nextConfig;
+// Uncommenting this makes the output folder try and be generated, but
+// it fails in ways I do not currently understand:
+// module.exports = nextConfig;
--- src/app/(posts)/post/[post_date]/[post_title]/page.tsx
+++ src/app/(posts)/post/[post_date]/[post_title]/page.tsx
@@ -25,28 +25,29 @@ type PostStaticParams = {
 	post_title: string;
 };
 
-export async function generateStaticParams() {
+export function generateStaticParams() {
 	const posts = get_posts();
 
-	const results = await Promise.allSettled(
-		posts
-			.map((post): string => {
-				return readFileSync(post, "utf8");
-			})
-			.map(async (file: string): Promise<PostStaticParams> => {
-				const { content, frontmatter } = await compileMDX<PostFrontmatter>({
-					source: file,
-					options: { parseFrontmatter: true },
-				});
+	// const results = await Promise.allSettled(
+	// 	posts
+	// 		.map((post): string => {
+	// 			return readFileSync(post, "utf8");
+	// 		})
+	// 		.map(async (file: string): Promise<PostStaticParams> => {
+	// 			const { content, frontmatter } = await compileMDX<PostFrontmatter>({
+	// 				source: file,
+	// 				options: { parseFrontmatter: true },
+	// 			});
 
-				return {
-					post_date: frontmatter.date,
-					post_title: frontmatter.repo_folder,
-				};
-			}),
-	);
+	// 			return {
+	// 				post_date: frontmatter.date,
+	// 				post_title: frontmatter.repo_folder,
+	// 			};
+	// 		}),
+	// );
 
-	return results.map((result) => result);
+	// return results.map((result) => result);
+	return [{ post_date: "2021-01-25", post_title: "learning-cpp" }];
 }
 
 export default async function Page({
